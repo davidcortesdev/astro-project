ffc9ea38d9749595a94bbd0f5494b8c6e5035d3a
David Cortés <daviidcl11@gmail.com>
2024-09-24 22:44:01 +0200

    config

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://davidcortesdev/github.io',
-  base: 'astro-project'
+  site: 'https://davidcortesdev/github.io/astro-project/',
+  base: '/astro-project/'
 });
